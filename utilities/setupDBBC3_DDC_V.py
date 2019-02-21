82d69eddb65b377b49427e2bd77bc0876e6a056d
oper <oper@StationUnit7.haystack.mit.edu>
2019-02-21 20:21:02 +0000

    Code cleanup

--- utilities/setupDBBC3_DDC_V.py
+++ utilities/setupDBBC3_DDC_V.py
@@ -11,7 +11,7 @@ from time import sleep
 
 if __name__ == "__main__":
 
-	#try:
+	try:
 
                 config = DBBC3Config()
 
@@ -34,83 +34,12 @@ if __name__ == "__main__":
  			val.validateSamplerOffsets(board)
 		
 
-		sys.exit(0)
-
-		#print "=== Disabling calibration loop"
-		#dbbc3.disableloop()
-	
-                val = DBBC3Validation(dbbc3, ignoreErrors=True)
-		val.validateSynthesizerLock(0)
-		val.validateSynthesizerLock(1)
-		val.validateSynthesizerLock(2)
-		val.validateSynthesizerLock(3)
- 
-		val.validateSynthesizerFreq(0)
-		val.validateSynthesizerFreq(1)
-		val.validateSynthesizerFreq(2)
-		val.validateSynthesizerFreq(3)
-
-		val.validateIFLevel('a')
-		val.validateIFLevel('b')
-		val.validateIFLevel('c')
-		val.validateIFLevel('d')
-
-		val.validateSamplerPhases()
-
- 		val.validateSamplerPower(0)
- 		val.validateSamplerPower(1)
-		val.validateSamplerPower('C')
-		val.validateSamplerPower('D')
-
- 		val.validateSamplerOffsets(1)
- 		val.validateSamplerOffsets(2)
-		val.validateSamplerOffsets('C')
-		val.validateSamplerOffsets('D')
-
-		
-		# load tap filters (extra script)
-		response = raw_input("Do you want to set the tap filters now? [y/n]")
-		if response == "y":
- 			print "=== Loading tap filters for board A"
- 			dbbc3.tap(1,"2000-4000_floating.flt")
- 			dbbc3.tap2(1,"0-2000_floating.flt")
- 			print "=== Loading tap filters for board B"
- 			dbbc3.tap(2,"2000-4000_floating.flt")
- 			dbbc3.tap2(2,"0-2000_floating.flt")
-			print "=== Loading tap filters for board C"
-			dbbc3.tap(2,"2000-4000_floating.flt")
-			dbbc3.tap2(2,"0-2000_floating.flt")
-			print "=== Loading tap filters for board D"
-			dbbc3.tap(3,"2000-4000_floating.flt")
-			dbbc3.tap2(3,"0-2000_floating.flt")
-		else:	
-			sys.exit(0)
-
-		print "=== Setting up calibration loop"
-		dbbc3.enablecal()
-		print "=== Enabling calibration loop"
-		dbbc3.enableloop()
-
-		print "=== Waiting for 1 minute to allow adjusting the power levels"
-		sleep(60)
-		
-		print "=== Now re-checking the bit statistics (should be proper 2-bit)"
- 		val.validateSamplerOffsets(0)
- 		val.validateSamplerOffsets(1)
-		val.validateSamplerOffsets(2)
-		val.validateSamplerOffsets(3)
-
-		print "=== Setting up calibration loop"
-		dbbc3.enablecal()
-		print "=== Enabling calibration loop"
-		dbbc3.enableloop()
-		
 		dbbc3.disconnect()
 		print "=== Done"
 
- #	except Exception as e:
- #		print e.message
- #		dbbc3.disconnect()
+ 	except Exception as e:
+ 		print e.message
+ 		dbbc3.disconnect()
 		
 
 	
