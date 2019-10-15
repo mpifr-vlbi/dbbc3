c5488e4e34db2add33fce1c331c4fb22bcaac1e4
rottmann@mpifr-bonn.mpg.de <rottmann@mpifr-bonn.mpg.de>
2019-10-15 12:49:23 +0000

    Add utility class

---
+++ lib/dbbc3/DBBC3Util.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+'''
+  This module is part of the DBBC3 package and implements the command sets
+  of the DBBC3 for the various modes and versions
+
+  Copyright (C) 2019 Helge Rottmann, Max-Planck-Institut für Radioastronomie, Bonn, Germany
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+__author__ = "Helge Rottmann"
+__copyright__ = "2019, Max-Planck-Institut für Radioastronomie, Bonn, Germany"
+__contact__ = "rottman[at]mpifr-bonn.mpg.de"
+__license__ = "GPLv3"
+
+from datetime import datetime
+import sys
+import re
+
+
+def parseTimeResponse(response):
+
+    year = 0
+    doy = 0
+    hour = 0
+    minute = 0
+    second = 0
+
+    timestamp = None
+
+    for line in response.split("\n"):
+        line = line.strip()
+        tok = line.split("=")
+
+        if tok[0].strip() == "halfYearsSince2000":
+            year = int(tok[1]) /2 + 2000
+        elif tok[0].strip() == "seconds":
+            doy = int(int(tok[1]) / 86400)
+            remSecs = int(tok[1]) - doy * 86400
+            hour = remSecs / 3600
+            minute = (remSecs - hour*3600) / 60
+            second = (remSecs - hour*3600 - minute*60)
+
+    if year > 0:
+        timestamp = datetime.strptime("%s %s %s %s %s UTC" %(year, doy+1, hour, minute, second), "%Y %j %H %M %S %Z")
+
+    return(timestamp)
