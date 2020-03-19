76792caac1689190cd69981006500041c4e17900
rottmann@mpifr-bonn.mpg.de <rottmann@mpifr-bonn.mpg.de>
2020-03-19 10:09:28 +0000

    Make the script more general

--- utilities/monitorStability.py
+++ utilities/monitorStability.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import logging
 import argparse
 from dbbc3.DBBC3 import DBBC3
 from dbbc3.DBBC3Config import DBBC3Config
@@ -11,100 +12,174 @@ import numpy as np
 from datetime import datetime
 from time import sleep
 
-parser = argparse.ArgumentParser("Monitor and log the PPS delay values.")
-
-parser.add_argument("-p", "--port", default=4000, type=int, help="The port of the control software socket (default: 4000)")
-parser.add_argument("-m", "--mode", required=True, help="The current DBBC3 mode, e.g. OCT_D or DDC_V")
-parser.add_argument("-n", "--num-coreboards", default=8, type=int, help="The number of activated core boards in the DBBC3 (default 8)")
-parser.add_argument("-i", "--if", dest='boards', nargs="+", help="A list of core boards to be used for setup and validation. (e.g. -i A C E). If not specified will use all activated core boards.")
-parser.add_argument("--use-version", dest='ver', default= "", help="The version of the DBBC3 software.  Will assume the latest release version if not specified")
-parser.add_argument("--ignore-errors", action='store_true', help="Ignore any errors and continue with the validation")
-parser.add_argument("--reset-on-error", action='store_true', help="Reset ADB3L boards in case of sampler drifts")
-parser.add_argument('ipaddress', help="the IP address of the DBBC3 running the control software")
-
-args = parser.parse_args()
-log = None
+args = None
+logger = None
 dbbc3 = None
+resetPPS = False
+resetAdb = False
 
-try:
+def parseCommandLine():
 
-        print ("===Trying to connect to %s:%d" % (args.ipaddress, args.port))
-        dbbc3 = DBBC3(host=args.ipaddress, port=args.port, mode=args.mode, numBoards=args.num_coreboards, version=args.ver)
-        val = DBBC3Validation(dbbc3, ignoreErrors=args.ignore_errors)
-        
-        print ("===Connected")
-        
-        useBoards = []
-        if args.boards:
-            for board in args.boards:
-                useBoards.append(dbbc3.boardToDigit(board))
-        else:
-            for board in range(args.num_coreboards):
-                useBoards.append(dbbc3.boardToDigit(board)) 
+    parser = argparse.ArgumentParser("Monitor and log the PPS delay values.")
+
+    parser.add_argument("-p", "--port", default=4000, type=int, help="The port of the control software socket (default: %(default)s)")
+    parser.add_argument("-m", "--mode", required=True, help="The current DBBC3 mode, e.g. OCT_D or DDC_V")
+    parser.add_argument("-n", "--num-coreboards", default=8, type=int, help="The number of activated core boards in the DBBC3 (default %(default)s)")
+    parser.add_argument("-i", "--if", dest='boards', nargs="+", help="A list of core boards to be used for setup and validation. (e.g. -i A C E). If not specified will use all activated core boards.")
+    parser.add_argument("--interval", default=60, type=int, help="Cadence of monitoring in seconds (default: %(default)s)")
+    parser.add_argument("--use-version", dest='ver', default= "", help="The version of the DBBC3 software.  Will assume the latest release version if not specified")
+#    parser.add_argument("--ignore-errors", action='store_true', help="Ignore any errors and continue with the validation")
+    parser.add_argument("--on-sampler-error", dest='onSamplerError', choices=["reset","reload"], help="Action to execute on sampler error: reset will reset samplers, reload will reload the firmware")
+    parser.add_argument("--on-pps-error", dest='onPpsError', default="resync", choices=["resync","ignore"], help="Action to execute on PPS error. (default %(default)s)")
+    parser.add_argument("--reload-interval", dest='reloadInterval', type=int, help="The interval in seconds on which to automatically reload the firmware")
+    parser.add_argument('ipaddress', help="the IP address of the DBBC3 running the control software")
+
+    return(parser.parse_args())
+
+def resetPPSSync():
+    if (args.onPpsError == "resync"):
+        logger.info ("=== resyncing the PPS (pps_sync)")
+        dbbc3.pps_sync()
+
+def resetADB3L():
+    
+    if (args.onSamplerError == "reset"):
+        logger.info ("=== resetting all ADB3L boards (reseth)")
+        dbbc3.adb3l_reseth()
+
+    elif (args.onSamplerError == "reload"):
+        logger.info ("=== reloading firmware")
+        dbbc3.core3h_reconfigure()
 
 
-        log = open("stability_%s_%s.log" % (args.mode, datetime.now().strftime("%Y%m%d_%H%M%S")), "w")
+def testPPSSync(useBoards):
+    global resetPPS
 
-        testPPS = False
+    ppsState = []
+    for board in useBoards:
+        ret = dbbc3.pps_delay(board)
+        ppsState.append("OK")
 
-        if (args.mode.startswith("DDC")):
-            testPPS = True
+        if  (ret[0] > 2000):
+            resetPPS = True
+            ppsState[board] = "FAIL"
+        line = "pps_sync on board %d: %d %d %s" % (board, ret[0], ret[1], ppsState[board])
+        logger.debug(line)
+    logger.info("pps states  {}".format(ppsState))
+#
+def testSamplerSync(useBoards):
+    global resetAdb
 
-       # dbbc3.disableloop()
-        if (testPPS):
-            dbbc3.pps_sync()
+    samplerState = []
+    for board in useBoards:
+        ret = dbbc3.core3h_core3_corr(board)
 
-        ppsState = ""
-        samplerState = ""
-        while 1:
-            line = str(datetime.now())
-            resetPPS = False
-            resetAdb = False
-            for board in useBoards:
-                    if (testPPS):
-                        ret = dbbc3.pps_delay(board)
-                        line += " %d %d " % (ret[0], ret[1])
-                        ppsState = " PPS: OK "
-                        if  (ret[0] > 2000):
-                            resetPPS = True
-                            ppsState = " PPS: FAIL "
-    #
-                    ret = dbbc3.core3h_core3_corr(board)
-                    line += " %s %s %s " % (ret[0], ret[1], ret[2])
+        samplerState.append("OK")
+        if (ret[0] < 170000000) or (ret[1] < 170000000) or (ret[2] < 170000000):
+            resetAdb = True
+            samplerState[board] = "FAIL"
 
-                    samplerState = " Samplers: OK "
-                    if (ret[0] < 170000000) or (ret[1] < 170000000) or (ret[2] < 170000000):
-                        resetAdb = True
-                        samplerState = " Samplers: FAIL "
+        line = "sampler corr on board %d: %s %s %s %s" % (board, ret[0], ret[1], ret[2], samplerState[board])
+        logger.debug(line)
+    logger.info("sampler states  {}".format(samplerState))
 
-            line += ppsState + samplerState
-            log.write (line  + "\n")
-            print (line)
-            if (args.reset_on_error):
+def main():
+    global args
+    global logger
+    global dbbc3
+    global resetAdb
+    global resetPPS
+    
+    args = parseCommandLine()
+    
+    logger = logging.getLogger("monitorStability")
+    logger.setLevel(logging.DEBUG)
+    # create file handler
+    fh = logging.FileHandler("stability_%s_%s.log" % (args.mode, datetime.now().strftime("%Y%m%d_%H%M%S")))
+    # create console handler
+    ch = logging.StreamHandler()
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    try:
+
+            logger.debug ("=== Trying to connect to %s:%d" % (args.ipaddress, args.port))
+            dbbc3 = DBBC3(host=args.ipaddress, port=args.port, mode=args.mode, numBoards=args.num_coreboards, version=args.ver)
+            #val = DBBC3Validation(dbbc3, ignoreErrors=args.ignore_errors)
+            
+            logger.info ("=== Connected to %s:%d" % (args.ipaddress, args.port))
+            logger.info ("=== Test parameters: {}".format(args))
+            
+            useBoards = []
+            if args.boards:
+                for board in args.boards:
+                    useBoards.append(dbbc3.boardToDigit(board))
+            else:
+                for board in range(args.num_coreboards):
+                    useBoards.append(dbbc3.boardToDigit(board)) 
+
+            testPPS = False
+            testSamplers = True
+
+            if (args.mode.startswith("DDC")):
+                testPPS = True
+
+           # dbbc3.disableloop()
+            if (testPPS):
+                dbbc3.pps_sync()
+
+            reloadStart = datetime.now()
+
+            while 1:
+                line = ""
+                resetPPS = False
+                resetAdb = False
+
+                samplerState = []
+
+                # check for firmware reload
+                if (args.reloadInterval):
+                    if ((datetime.now() - reloadStart).total_seconds() > args.reloadInterval):
+                        logger.info("reloading firmware (currently disabled)")
+                        reloadStart = datetime.now()
+                        #dbbc3.reconfigure()
+
+                if (testSamplers):
+                    testSamplerSync(useBoards)
+
+                if (testPPS):
+                    testPPSSync(useBoards)
+
                 if (resetAdb):
-                    log.write (" # Resetting all ADB3L boards\n")
-                    print ("# Resetting all ADB3L boards")
-                    dbbc3.adb3l_reset()
+                    if (args.onSamplerError):
+                        resetADB3L()
+                        resetPPS= True
+
                     resetAdb = False
-                    resetPPS= True
+
                 if (resetPPS):
-                    log.write (" # Resyncing the PPS\n")
-                    print ("# Resyncing the PPS")
-                    dbbc3.pps_sync()
+                    resetPPSSync()
                     resetPPS= False
-            sleep(60)
+                    
 
-        dbbc3.disconnect()
-        print ("=== Done")
 
-except Exception as e:
-        print ("ERROR: ", e)
-        
-finally:
-        if (dbbc3):
+                sleep(args.interval)
+
             dbbc3.disconnect()
-        if log:
-            log.close()
-        
+            logger.info ("=== Done")
 
+    except Exception as e:
+            print ("ERROR: ", e)
+            
+    finally:
+            if (dbbc3):
+                dbbc3.disconnect()
+            
 
+if __name__ == "__main__":
+    main()
