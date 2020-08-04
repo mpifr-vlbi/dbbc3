5f49d8808b9ad3e6b169a37ddc2384e2b682dd95
rottmann@mpifr-bonn.mpg.de <rottmann@mpifr-bonn.mpg.de>
2020-08-04 14:16:46 +0000

    Adapt to changes in DBBC3 class structure

--- utilities/setupDBBC3_DDC_V.py
+++ utilities/setupDBBC3_DDC_V.py
@@ -12,32 +12,27 @@ from time import sleep
 
 parser = argparse.ArgumentParser(description="Setup and validate DBBC3 in DDC_V mode")
 
-parser.add_argument("-p", "--port", default=4000, type=int, help="The port of the control software socket (default: 4000)")
-parser.add_argument("-n", "--num-coreboards", default=8, type=int, help="The number of activated core boards in the DBBC3 (default 8)")
+parser.add_argument("-p", "--port", default=4000, type=int, help="The port of the control software socket (default: %(default)s)")
+parser.add_argument("-n", "--num-coreboards", default=8, type=int, help="The number of activated core boards in the DBBC3 (default: %(default)s)")
 parser.add_argument("-i", "--if", dest='boards', nargs="+", help="A list of core boards to be used for setup and validation. (e.g. -i A C E). If not specified will use all activated core boards.")
 parser.add_argument("--use-version", dest='ver', default= "", help="The software version of the DBBC3 DDC_V mode to use. Will assume the latest release version if not specified")
-parser.add_argument("--ignore-errors", action='store_true', help="Ignore any errors and continue with the validation")
+parser.add_argument("--ignore-errors", dest='ignoreErrors',default=False, action='store_true', help="Ignore any errors and continue with the validation")
 parser.add_argument('ipaddress', help="the IP address of the DBBC3 running the control software")
+parser.add_argument("-m", "--mode", required=False, default="DDC_V", help="The current DBBC3 mode (default: %(default)s)")
 
 args = parser.parse_args()
 
 try:
 
-        config = DBBC3Config()
-
-        config.numCoreBoards = args.num_coreboards
-
-        config.host=args.ipaddress
-        config.port=args.port
-
-        dbbc3 = DBBC3(config, mode="DDC_V", version=args.ver)
-        val = DBBC3Validation(dbbc3, ignoreErrors=args.ignore_errors)
-        
-        print ("===Trying to connect to %s:%d" % (config.host, config.port))
-        dbbc3.connect()
+        print ("===Trying to connect to %s:%d" % (args.ipaddress, args.port))
+        dbbc3 = DBBC3(host=args.ipaddress, port=args.port, mode=args.mode)
         print ("===Connected")
+
+        ver = dbbc3.version()
+        print ("=== DBBC3 is running: mode=%s version=%s(%s)" % (ver['mode'], ver['majorVersion'], ver['minorVersion']))
+       # print (dbbc3.version())
         
-        #val.validatePPS()
+        val = DBBC3Validation(dbbc3, ignoreErrors=args.ignoreErrors)
 
         useBoards = []
         if args.boards:
@@ -47,9 +42,9 @@ try:
             for board in range(args.num_coreboards):
                 useBoards.append(dbbc3.boardToDigit(board)) 
 
-        print (dbbc3.version())
 
         for board in useBoards:
+                print ("=== Processing board %d" % (board))
                 val.validatePPSDelay(board)
                 val.validateTimesync(board)
                 val.validateSynthesizerLock(board)
@@ -63,6 +58,6 @@ try:
         print ("=== Done")
 
 except Exception as e:
-        print (e.message)
+        print (e)
         if 'dbbc3' in vars() or 'dbbc3' in globals():
                 dbbc3.disconnect()
