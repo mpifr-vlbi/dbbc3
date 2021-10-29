606ef4e4c64c458205e558b1726c5b65dd195a65
rottmann@mpifr-bonn.mpg.de <rottmann@mpifr-bonn.mpg.de>
2021-10-29 10:01:11 +0000

    add count and total power plots

--- utilities/multicastGUI.py
+++ utilities/multicastGUI.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import copy
+import collections
 import time
 import argparse
 import json
@@ -11,9 +12,14 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from dbbc3.DBBC3Multicast import DBBC3Multicast
 from datetime import datetime
+import numpy as np
+from matplotlib.figure import Figure
+from random import seed, gauss
 
 
 from tkinter import *
+from tkinter import font
+from tkinter import ttk
 
 args = None
 
@@ -21,9 +27,19 @@ def thread_mcPoll(name, mc, q):
     while True:
         print ("polling ", datetime.now())
         res = mc.poll()
+
         res["time"] = datetime.now()
+
+        seed(datetime.now().microsecond)
+
+        # estimate total power from count values
+        for board in range(1,9):
+            if (args.simulate):
+                res["if_{}".format(board)]["count"] = int(32000 + gauss(0, 1000))
+            res["if_{}".format(board)]["power"] = float(mc.message["if_{}".format(board)]["count"]) / 1420.0 + float(mc.message["if_{}".format(board)]["attenuation"]) / 2.0
+        
         q.put(res)
-        print ("result: ", mc.message["if_1"]["mode"])
+        #print ("result: ", mc.message["if_1"]["mode"])
 
 class GenericWindow(object):
     zest = "" 
@@ -52,6 +68,14 @@ class MainWindow(GenericWindow):
         self.messageVars={}
         self.activeBoards = [1,2,3,4]
 
+        self.counts = {} 
+        self.power = {} 
+        self.pltCounts = {}
+        self.pltPower = {}
+        for board in self.activeBoards:
+            self.counts[board] = collections.deque(maxlen=100)
+            self.power[board] = collections.deque(maxlen=100)
+        
         self.mc = DBBC3Multicast(args.group, args.port, args.timeout)
 
         # start multicast polling in its own thread
@@ -63,15 +87,6 @@ class MainWindow(GenericWindow):
         self.message = self.q.get(block=True)
 
         self._initVars(self.message)
-        #print (self.message)
-
-        #self.messageVars = copy.deepcopy(self.message)
-        #for k, v in self.messageVars.items():
-        #    print (k, v)
-        #    if isinstance(v, str):
-        #        self.messageVars[k] = StringVar(value=v)
-        #print (self.messageVars)
-
         self._setupWidgets()
         self.update()
         self.root.mainloop()
@@ -96,134 +111,26 @@ class MainWindow(GenericWindow):
         message = self.q.get(block=True)
         self._initVars(message)
         self._setWidgetStates()
+
+        
+        #print(list(range(-1*len(self.counts)+1,1,1)))
+        #print (list(self.counts))
+        #self.pltCounts.set_xdata(list(range(-1*len(self.counts))))
+        for board in self.activeBoards:
+            fig = self.pltPower[board].figure
+            self.pltCounts[board].set_xdata(list(range(-1*len(self.counts[board])+1,1,1)))
+            self.pltCounts[board].set_ydata(list(self.counts[board]))
+            self.pltPower[board].set_xdata(list(range(-1*len(self.power[board])+1,1,1)))
+            self.pltPower[board].set_ydata(list(self.power[board]))
+            #print (self.pltPower[board].figure.axes)
+        for axis in fig.axes:
+            axis.relim()
+            axis.autoscale_view()
+        self.canvasCounts.draw()
         #print (self.messageVars["if_1_count"].get())
         #print ("update")
         self.root.after(1000, self.update)
     
-    def update2(self):
-
-            start = time.time()
-            message = self.q.get()
-            print (message["mode"])
-            print (message["majorVersion"])
-            print (message["minorVersion"])
-            print (message["minorVersionString"])
-            print (datetime.now(), message["if_1"]["mode"])
-
-            for board in self.activeBoards:
-                    self.varIfCounts[board-1].set(message["if_{0}".format(board)]["count"])
-                    self.varIfTarget[board-1].set(message["if_{0}".format(board)]["target"])
-                    self.varIfMode[board-1].set(message["if_{0}".format(board)]["mode"])
-                    self.varIfAtt[board-1].set(message["if_{0}".format(board)]["attenuation"])
-
-                    # downconversion state
-                    if message["if_{0}".format(board)]["synth"]['status'] == 1:
-                        dcState = "on"
-                    else:
-                        dcState = "off"
-                    self.varSynthState[board-1].set(dcState )
-
-                    # synthesizer lock state
-                    if dcState == "off":
-                        state = ""
-                        freq = ""
-                        bgcolor = self.lblSynthFreq[board-1]['bg']
-                    else:
-                        freq = message["if_{0}".format(board)]["synth"]['frequency']*2
-                        if message["if_{0}".format(board)]["synth"]['lock'] == 1:
-                            state = "locked"
-                            bgcolor = "green"
-                        else:
-                            state = "unlocked"
-                            bgcolor = "red"
-                    self.lblSynthLock[board-1].config(bg=bgcolor)
-                    self.varSynthLock[board-1].set(state)
-                    self.varSynthFreq[board-1].set(freq)
-
-                    #color code agc settings
-                    if message["if_{0}".format(board)]["mode"] != "agc":
-                        self.lblMode[board-1].config(bg="red")
-                    else:
-                        self.lblMode[board-1].config(bg="green")
-
-                    # color code attenuation
-                    if abs(message["if_{0}".format(board)]["attenuation"] - 31) < 10:
-                        self.lblAtt[board-1].config(bg="green")
-                    elif abs(message["if_{0}".format(board)]["attenuation"] - 31) < 15:
-                        self.lblAtt[board-1].config(bg="orange")
-                    elif abs(message["if_{0}".format(board)]["attenuation"] - 31) < 20:
-                        self.lblAtt[board-1].config(bg="red")
-                    
-                    # sampler stat plots
-                    stats0 = list(message["if_{0}".format(board)]["sampler0"]["stats"])
-                    stats1 = list(message["if_{0}".format(board)]["sampler1"]["stats"])
-                    stats2 = list(message["if_{0}".format(board)]["sampler2"]["stats"])
-                    stats3 = list(message["if_{0}".format(board)]["sampler3"]["stats"])
-                    perc0 = list(message["if_{0}".format(board)]["sampler0"]["statsFrac"])
-                    perc1 = list(message["if_{0}".format(board)]["sampler1"]["statsFrac"])
-                    perc2 = list(message["if_{0}".format(board)]["sampler2"]["statsFrac"])
-                    perc3 = list(message["if_{0}".format(board)]["sampler3"]["statsFrac"])
-                    canvas0 = self.lblSampler0Stats[board-1]
-                    canvas1 = self.lblSampler1Stats[board-1]
-                    canvas2 = self.lblSampler2Stats[board-1]
-                    canvas3 = self.lblSampler3Stats[board-1]
-                    self.pltSampler0Stats[board-1].clear()
-                    self.pltSampler0Stats[board-1].bar(['--','-','+','++'], stats0)
-                    for index, value in enumerate(stats0):
-                        print (index, value)
-                        self.pltSampler0Stats[board-1].text(index-0.3, value, str(round(perc0[index],0)), fontsize='xx-small')
-
-                    #self.pltSampler1Stats[board-1].clear()
-                    #self.pltSampler1Stats[board-1].bar(['--','-','+','++'], stats1)
-                    #for index, value in enumerate(stats1):
-                    #    print (index, value)
-                    #    self.pltSampler1Stats[board-1].text(index-0.3, value, str(round(perc1[index],0)), fontsize='xx-small')
-                    #self.pltSampler2Stats[board-1].clear()
-                    #self.pltSampler2Stats[board-1].bar(['--','-','+','++'], stats2)
-                    #for index, value in enumerate(stats2):
-                    #    print (index, value)
-                    #    self.pltSampler2Stats[board-1].text(index-0.3, value, str(round(perc2[index],0)), fontsize='xx-small')
-                    #self.pltSampler3Stats[board-1].clear()
-                    #self.pltSampler3Stats[board-1].bar(['--','-','+','++'], stats3)
-                    #for index, value in enumerate(stats3):
-                    #    print (index, value)
-                    #    self.pltSampler3Stats[board-1].text(index-0.3, value, str(round(perc3[index],0)), fontsize='xx-small')
-                    canvas0.draw()
-                    canvas1.draw()
-                    canvas2.draw()
-                    canvas3.draw()
-                    
-                    #print (message["if_{0}".format(board)]["count"])
-                    #print (message["if_{0}".format(board)]["target"])
-                    #print (message["if_{0}".format(board)]["attenuation"])
-                    #print (message["if_{0}".format(board)]["mode"])
-                    # HR
-                    #print ("tsys: ", message["if_{0}".format(board)]["tsys"])
-                    #print ("delayCorr: ", message["if_{0}".format(board)]["delayCorr"])
-                    #print ("synth: ", message["if_{0}".format(board)]["synth"])
-                    #print ("tpOn: ", message["if_{0}".format(board)]["tpOn"])
-                    #print ("tpOff: ", message["if_{0}".format(board)]["tpOff"])
-                    #print ("time: ", message["if_{0}".format(board)]["time"])
-                    #print ("ppsDelay: ", message["if_{0}".format(board)]["ppsDelay"])
-                    #print (message["if_{0}".format(board)].keys())
-                    #print (message["if_1"]["bbc_1"].keys())
-
-                    #for sNum in range(4):
-                    #        print (message["if_{0}".format(board)]["sampler{0}".format(sNum)])
-
-                    #HRfor bbc in range(8):
-                        # first eight BBCs
-                    #    print ("bbc_{0}".format(bbc+1+(int(board)-1)*8), message["if_{0}".format(board)]["bbc_{0}".format(bbc+1+(int(board)-1)*8)])
-                        # second eight BBCs
-                        #print ("bbc_{0}".format(bbc+65+(int(board)-1)*8), message["if_{0}".format(board)]["bbc_{0}".format(bbc+65+(int(board)-1)*8)])
-            end = time.time()
-            print("Execution time: " , end - start)        
-            self.root.after(0, self.update)
-
-
-    def my_callback(self, var, indx, mode):
-        print ("Traced variable {} {} {}".format(var, indx, mode))
-
     def _setWidgetStates(self):
 
         for board in self.activeBoards:
@@ -241,7 +148,6 @@ class MainWindow(GenericWindow):
             else:
                 self.messageVars[key].set(str(train[-1]))
 
-        # special treatment of a few fields
         for board in self.activeBoards:
             # synthesizer status
             if self.messageVars["if_{}_synth_status".format(board)].get() == '1':
@@ -257,6 +163,9 @@ class MainWindow(GenericWindow):
             # synthesizer lock
             freq = float(self.messageVars["if_{}_synth_frequency".format(board)].get()) * 2
             self.messageVars["if_{}_synth_frequency".format(board)].set(str(freq))
+            self.counts[board].append(int(self.messageVars["if_{}_count".format(board)].get()))
+            #print (self.messageVars["if_{}_power".format(board)].get())
+            self.power[board].append(float(self.messageVars["if_{}_power".format(board)].get()))
         
         
 
@@ -309,6 +218,9 @@ class MainWindow(GenericWindow):
         frmBBC = LabelFrame(self.root, text="BBCs")
         frmBBC.grid(row=40,column=0, sticky=E+W+N+S, padx=2,pady=2)
 
+        #frmNotebook = Frame(self.root)
+        #frmNotebook.grid(row=0, column=10, sticky=E+W+S+N)
+
         #self.lblCount = []
         #self.lblTarget = []
          
@@ -350,7 +262,47 @@ class MainWindow(GenericWindow):
         Label(frmSampler, text="sampler 1 stats").grid(row=3,column=0, sticky=W)
         Label(frmSampler, text="sampler 2 stats").grid(row=4,column=0, sticky=W)
         Label(frmSampler, text="sampler 3 stats").grid(row=5,column=0, sticky=W)
-    
+
+        #frmNotebook
+        self.notebook = ttk.Notebook(self.root)
+        self.notebook.grid(row=10, column=10, rowspan=100, sticky=E+W+S+N)
+
+        frameIF = ttk.Frame(self.notebook, width=400, height=280)
+        frameIF.grid(row=0,column=0,sticky=E+W+S+N)
+
+        # count plot
+        fig,axes = plt.subplots(2,1,constrained_layout=True)
+        for board in self.activeBoards:
+            self.pltCounts[board], = axes[0].plot([0],[32000], label=str(board))
+            self.pltPower[board], = axes[1].plot([0],[1000], label=str(board))
+
+        axes[0].set_title ("Counts", fontsize=16)
+        axes[0].set_ylabel("counts", fontsize=11)
+        axes[0].set_xlabel("time[s]", fontsize=11)
+#        axes[0].set_ylim([30000,40000])
+        axes[0].set_xlim([-100,0])
+        axes[0].grid(True)
+        axes[0].legend(fontsize='x-small', loc=2)
+        axes[1].set_title ("Total power", fontsize=16)
+        axes[1].set_ylabel("dBm", fontsize=11)
+        axes[1].set_xlabel("time[s]", fontsize=11)
+#        axes[1].set_ylim([0,100])
+        axes[1].set_xlim([-100,0])
+        axes[1].grid(True)
+        axes[1].legend(fontsize='x-small', loc=2)
+
+        self.canvasCounts = FigureCanvasTkAgg(fig, master=frameIF)
+        self.canvasCounts.get_tk_widget().pack()
+        self.canvasCounts.draw()
+
+        self.notebook.add(frameIF, text='Total Power') 
+        
+        
+        style = ttk.Style()
+        myfont = font.Font(family="Courier", size=11)
+        style.configure("My.TButton",font=myfont, foreground="black", relief="sunken", background="white", justify="center")
+        style.map( "My.TButton", foreground=[("disabled", "black")], justify=[("disabled", "center")])
+        
         for i in range(len(self.activeBoards)):
             b = self.activeBoards[i]
             frmIf.columnconfigure(i,minsize=100)
@@ -362,10 +314,10 @@ class MainWindow(GenericWindow):
             Label(frmSampler, text=str(i)).grid(row=1,column=i+1, sticky=E+W)
 
             #frmIf
-            self.messageComp["if_{}_count".format(b)] = Button(frmIf, relief=SUNKEN,state=DISABLED, disabledforeground="black", textvariable=self.messageVars["if_{}_count".format(b)])
-            self.messageComp["if_{}_target".format(b)] = Button(frmIf, relief=SUNKEN,state=DISABLED, disabledforeground="black", textvariable=self.messageVars["if_{}_target".format(b)])
-            self.messageComp["if_{}_mode".format(b)] = Button(frmIf, relief=SUNKEN,state=DISABLED, disabledforeground="black", textvariable=self.messageVars["if_{}_mode".format(b)])
-            self.messageComp["if_{}_attenuation".format(b)] = Button(frmIf, relief=SUNKEN,state=DISABLED, disabledforeground="black", textvariable=self.messageVars["if_{}_mode".format(b)])
+            self.messageComp["if_{}_count".format(b)] = ttk.Button(frmIf, style="My.TButton", state=DISABLED, textvariable=self.messageVars["if_{}_count".format(b)])
+            self.messageComp["if_{}_target".format(b)] = ttk.Button(frmIf, style="My.TButton", state=DISABLED, textvariable=self.messageVars["if_{}_target".format(b)])
+            self.messageComp["if_{}_mode".format(b)] = ttk.Button(frmIf, style="My.TButton", state=DISABLED, textvariable=self.messageVars["if_{}_mode".format(b)])
+            self.messageComp["if_{}_attenuation".format(b)] = ttk.Button(frmIf, style="My.TButton", state=DISABLED, textvariable=self.messageVars["if_{}_attenuation".format(b)])
 
             self.messageComp["if_{}_count".format(b)].grid(row=2,column=i+1, sticky=E+W)
             self.messageComp["if_{}_target".format(b)].grid(row=3,column=i+1, sticky=E+W)
@@ -419,6 +371,7 @@ def parseCommandLine():
     parser.add_argument("-b", "--boards", dest='boards', type=lambda s: list(map(str, s.split(","))), help="A comma separated list of core boards to be used for setup and validation. Can be specified as 0,1 or A,B,.. (default: use all activated core boards)")
 #    parser.add_argument("-m", "--mode", required=False, help="The current DBBC3 mode, e.g. OCT_D or DDC_V")
     parser.add_argument("-t", "--timeout", required=False, help="The maximum number of seconds to wait for a multicast message.")
+    parser.add_argument("-s", "--simulate", action='store_true', required=False, help="Produce simulated IF counts.")
 #    parser.add_argument("--use-version", dest='ver', default= "", help="The version of the DBBC3 software.  Will assume the latest release version if not specified")
 
     return(parser.parse_args())
