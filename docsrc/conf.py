26aebc849a20036b26c12140fbaa14c0b61886e3
rottmann@mpifr-bonn.mpg.de <rottmann@mpifr-bonn.mpg.de>
2022-12-10 16:12:15 +0000

    test of readthedocs

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -93,12 +93,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'classic'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'body_max_width': 'none', 'sidebarwidth': 300}
+#html_theme_options = {'body_max_width': 'none', 'sidebarwidth': 300}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
